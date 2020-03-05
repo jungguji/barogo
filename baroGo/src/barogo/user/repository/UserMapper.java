23321f8c46965185b2f31a231db7e33b22f58ba1
junggu <junggu.ji.dev@gmail.com>
2020-03-05 20:30:51 +0900

    type : Feat
    유저 검색 쿼리 추가

--- baroGo/src/barogo/user/repository/UserMapper.java
+++ baroGo/src/barogo/user/repository/UserMapper.java
@@ -1,19 +1,26 @@
-package barogo.login.repository;
+package barogo.user.repository;
 
-import java.util.Map;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import useInfo.UserVO;
+
 @Mapper
-public interface LoginMapper {
+public interface UserMapper {
 
     @Select("SELECT password FROM admin WHERE id=#{userId}")
     String findPasswordById(@Param("userId") String userId);
     
-    
     @Insert("INSERT INTO admin VALUES(#{id}, #{password})")
     int createAdmin(@Param("id") String id, @Param("password") String password);
+    
+    @Select("SELECT * FROM user WHERE name=#{name}")
+    List<UserVO> findByName(@Param("name") String name);
+    
+    @Select("SELECT * FROM user WHERE userId=#{userId}")
+    UserVO findByUserId(@Param("userId") String userId);
 }
