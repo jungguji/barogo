02fb7ec998396a8f3d6faaf7e7f9d3426714ae42
JG.JI <junggu.ji.dev@gmail.com>
2020-03-08 15:42:04 +0900

    type : Fix
    불필요한 파일 제거

--- baroGo/src/adminCalculate/CalcPopup.java
+++ baroGo/src/adminCalculate/CalcPopup.java
@@ -2,7 +2,6 @@ package adminCalculate;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
